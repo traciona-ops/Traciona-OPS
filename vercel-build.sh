ac5cb7ace28184c743867a788fa9f3fa3100be48
Adriano <adrianottksa@gmail.com>
2026-08-08 20:37:09 -0300

    disable: migrations in vercel build (run manually after deploy)

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "=== Traciona: Running migrations ==="
-npm run migrate:up
+# Migrations disabled during build (run manually after deploy)
+# echo "=== Traciona: Running migrations ==="
+# npm run migrate:up
 
 echo "=== Traciona: Building Next.js ==="
 npm run build
